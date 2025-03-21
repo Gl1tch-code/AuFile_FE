7243d0e0a1723bb819c603cb364188207f3ea83a
Gl1tch-code <ibrahimdrissi05@gmail.com>
2025-03-21 21:25:56 +0000

    use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "./core/context/ThemeContext.tsx";
 import Home from "./Pages/Home/index.tsx";
 import "./index.css";
@@ -8,13 +8,13 @@ import RecentlyUploaded from "./Pages/RecentlyUploaded/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
           <Routes>
             <Route element={<Home />} path="/" />
             <Route element={<RecentlyUploaded />} path="/recently-uploaded" />
           </Routes>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
