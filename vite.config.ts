8e23ce06fabf01f544f07effecccf23fdd3e51e1
Gl1tch-code <ibrahimdrissi05@gmail.com>
2025-03-21 21:21:03 +0000

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/AuFile_FE/" : "/",
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Listen on all interfaces
+    host: "0.0.0.0", // Listen on all interfaces
     port: 5173, // Ensure the port is correct
-  }
-})
+  },
+}));
